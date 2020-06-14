c0162237887aadb116f9cf407f57928da514daea
= <=>
2020-06-14 20:03:43 +0900

    test code fix

--- test/calculate.spec.js
+++ test/calculate.spec.js
@@ -13,16 +13,19 @@ describe('test for calculate', () => {
   afterEach(() => {
     jest.restoreAllMocks()
   })
-  it('연봉을 입력하지 않고 버튼을 누르면 페이지가 이동되지 않는다.', function () {
-    wrapper.find('.btn-grad').trigger('click')
-    expect(wrapper.vm.$router.push).not.toHaveBeenCalled()
-  })
 
-  it('연봉을 입력하고 버튼을 누르면 페이지가 이동된다.', function () {
-    wrapper.setData({
-      salary: 5000
+  describe('interaction', () => {
+    it('연봉을 입력하지 않고 버튼을 누르면 페이지가 이동되지 않는다.', function () {
+      wrapper.find('.btn-grad').trigger('click')
+      expect(wrapper.vm.$router.push).not.toHaveBeenCalled()
     })
-    wrapper.find('.btn-grad').trigger('click')
-    expect(wrapper.vm.$router.push).toHaveBeenCalled()
+
+    it('연봉을 입력하고 버튼을 누르면 페이지가 이동된다.', function () {
+      wrapper.setData({
+        salary: 5000
+      })
+      wrapper.find('.btn-grad').trigger('click')
+      expect(wrapper.vm.$router.push).toHaveBeenCalled()
+    })
   })
 })
